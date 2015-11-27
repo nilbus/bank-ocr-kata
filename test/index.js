6aeaca38570939148d60c28d9dfceafc85b9f160
Edward Anderson <nilbus@nilbus.com>
2015-11-27 11:57:15 -0500

    Switch out Mocha for Jasmine 2.0

--- test/index.js
+++
@@ -1,8 +0,0 @@
-import assert from 'assert';
-import bankOcr from '../lib';
-
-describe('bank-ocr', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
-});
