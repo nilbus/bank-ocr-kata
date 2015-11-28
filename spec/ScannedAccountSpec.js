6e1795019b0966f9abbbc9e5429ec7c98fa2df29
Edward Anderson <nilbus@nilbus.com>
2015-11-27 20:42:51 -0500

    Implement ScannedAccount for User Story 1

---
+++ spec/ScannedAccountSpec.js
@@ -0,0 +1,20 @@
+import ScannedAccount from '../lib/ScannedAccount';
+import inputExample from './support/inputExample';
+
+describe('User Story 1:', function () {
+  const validAccount = inputExample(`
+    _  _     _  _  _  _  _
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|
+
+`);
+
+  describe('ScannedAccount', function () {
+    describe('toString', function () {
+      it('reutrns a string representing the account number', function () {
+        const scannedAccount = new ScannedAccount(validAccount);
+        expect(scannedAccount.toString()).toEqual('123456789');
+      });
+    });
+  });
+});
--- spec/support/inputExample.js
+++ spec/support/inputExample.js
@@ -1,4 +1,4 @@
-import assert from 'assert'
+import assert from 'assert';
 
 function inputExample(input) {
   let example = withoutLeadingNewline(input);
